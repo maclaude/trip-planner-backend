dbafd5ad3e5749857ff8114c3cd95e525d922d1b
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-06-06 16:18:20 +0200

    User authentication login

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@
  * NPM import
  */
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 /**
  * Local import
@@ -41,4 +42,45 @@ exports.signup = async (req, res, next) => {
   }
 };
 
-exports.login = (req, res, next) => {};
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    // Finding current user by email
+    const user = await User.findOne({ email });
+    // Throw an error if nothing is retrieved
+    if (!user) {
+      const error = new Error('No user found with this email');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    // Comparing passwords
+    const passwordsMatched = await bcrypt.compare(password, user.password);
+    // Throw an error if passwords don't match
+    if (!passwordsMatched) {
+      const error = new Error('Password is incorrect');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    // Generating token
+    const token = jwt.sign(
+      {
+        email: user.email,
+        userId: user._id.toString(),
+      },
+      'secret',
+      { expiresIn: '1h' }
+    );
+
+    // Sending the response to the client
+    res.status(200).json({ token, userId: user._id.toString() });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    // Next to reach the error middleware
+    next(err);
+  }
+};
