5b572c5e8d2c77e2df431fe78bcecb5e0808366c
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-06-06 16:07:45 +0200

    User authentication signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,44 @@
 // Auth controller
 
 /**
+ * NPM import
+ */
+const bcrypt = require('bcryptjs');
+
+/**
+ * Local import
+ */
+// Models
+const User = require('../models/user');
+
+/**
  * Code
  */
-exports.signup = (req, res, next) => {};
+exports.signup = async (req, res, next) => {
+  const { firstname, lastname, email, password } = req.body;
+
+  try {
+    // Encrypting password
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    // Creating new user
+    const newUser = await new User({
+      firstname,
+      lastname,
+      email,
+      password: hashedPassword,
+    });
+    const response = await newUser.save();
+
+    // Sending the response to the client
+    res.status(201).json({ message: 'User created', userId: response._id });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    // Next to reach the error middleware
+    next(err);
+  }
+};
 
 exports.login = (req, res, next) => {};
