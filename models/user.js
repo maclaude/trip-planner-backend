19fae826143f40727b41ba85f7b5e828b4db75b7
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-06-12 16:13:55 +0200

    User model updated

--- models/user.js
+++ models/user.js
@@ -8,24 +8,37 @@ const mongoose = require('mongoose');
  */
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  firstname: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+    projects: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Project',
+      },
+    ],
   },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  // createdAt - updatedAt
+  { timestamps: true }
+);
 
 /**
  * Export
